12471c570190a03d94c61520be91e2e98001a9cb
Caleb Creed <thirdcreed@gmail.com>
2020-02-16 23:58:31 -0600

    various server tests

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
+const express = require('express');
+const app = express();
+const http = require('http')
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  
+});
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  console.log(req.files)
+});
+
+http.createServer(app).listen(80)
+
 
 const typeDefs = `
   type Query {
@@ -11,6 +33,7 @@ const resolvers = {
     hello: (_, args) => `Hello ${args.name || 'World'}!`,
   },
 }
+
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log(`Server is running at http://localhost:4000`))
