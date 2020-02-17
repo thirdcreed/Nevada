e5e6152cf43d8250ce492c702891368030ab7aa2
Caleb Creed <thirdcreed@gmail.com>
2020-02-17 00:26:53 -0600

    adds https listener

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 const { GraphQLServer } = require('graphql-yoga')
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
+
 const http = require('http')
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
@@ -10,6 +12,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log(db)
 });
+console.log('sane')
+app.use(fileUpload());
 
 app.post('/upload', function(req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
